19e3e74c9c5f0cddb14ee5d57ffb4f4d6a15c0ec
happyzrt@aliyun <xsfzrt@gmail.com>
2022-04-24 22:35:55 +0800

    add absolute path

--- architecture/tunnel/script/server.sh
+++ architecture/tunnel/script/server.sh
@@ -1,5 +1,5 @@
-sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
-echo 'net.ipv4.ip_forward=1'>>/etc/sysctl.conf
-sysctl -p
-iptables -t nat -D POSTROUTING -s 192.168.13.0/24 -o eth0 -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 192.168.13.0/24 -o eth0 -j MASQUERADE
+/usr/bin/sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+/usr/bin/echo 'net.ipv4.ip_forward=1'>>/etc/sysctl.conf
+/usr/sbin/sysctl -p
+/usr/sbin/iptables -t nat -D POSTROUTING -s 192.168.13.0/24 -o eth0 -j MASQUERADE
+/usr/sbin/iptables -t nat -A POSTROUTING -s 192.168.13.0/24 -o eth0 -j MASQUERADE
