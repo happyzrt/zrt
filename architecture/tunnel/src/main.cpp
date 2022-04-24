5461656bb8f0ef629e4a9f41cc28c1e9d92d76cf
happyzrt@aliyun <xsfzrt@gmail.com>
2022-04-24 22:23:39 +0800

    add sigwait

--- architecture/tunnel/src/main.cpp
+++ architecture/tunnel/src/main.cpp
@@ -1,24 +1,41 @@
+#include <unistd.h>
+#include <signal.h>
 #include <iostream>
 #include <vector>
 #include "tunnel_factory.h"
 
+tunnel* tun = nullptr;
+void sig_handle(int num)
+{
+    if (tun != nullptr) {
+        tun->stop_work();
+        tun = nullptr;
+    }
+}
 int main(int argc, char* argv[])
 {
     std::vector<std::string> para;
     for (int i = 1; i < argc; i++) {
         para.emplace_back(std::string(argv[i]));
     }
-    tunnel* tun = tunnel_factory::instance().get_tunnel(para);
+    tun = tunnel_factory::instance().get_tunnel(para);
     if (tun == nullptr) {
         std::cout << "create tunnel failed" << std::endl;
         return 0;
     }
 
-    if (tun->start_work()) {
-        int tmp;
-        std::cin >> tmp;
-    }
-    
-    tun->stop_work();
+    tun->start_work();
+    struct sigaction newact;
+    newact.sa_handler = sig_handle;
+    sigemptyset(&newact.sa_mask);
+    newact.sa_flags = 0;
+    sigaction(SIGINT, &newact, nullptr);
+
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    int num;
+    sigwait(&set, &num);
+    std::cout << num << std::endl;
     return 0;
 }
