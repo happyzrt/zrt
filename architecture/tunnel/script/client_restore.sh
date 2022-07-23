3cd6b31139066ee90b2160244fcdf053eaf0e8db
happyzrt <xsfzrt@gmai.com>
2022-07-24 01:57:59 +0800

    add dns server cmd for osx

--- architecture/tunnel/script/client_restore.sh
+++ architecture/tunnel/script/client_restore.sh
@@ -2,3 +2,4 @@ gate_way=`/usr/sbin/netstat -rn -f inet|/usr/bin/grep "47.91.225.66"|/usr/bin/aw
 /sbin/route delete default
 /sbin/route -n add  default ${gate_way}
 /sbin/route delete  47.91.225.66 -gateway ${gate_way}
+/usr/sbin/networksetup -setdnsservers WI-FI ${gate_way}
